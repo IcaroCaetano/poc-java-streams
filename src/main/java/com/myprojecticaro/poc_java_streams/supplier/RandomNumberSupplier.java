aa45d93ac373a260d3fab58272d6442e28fb371a
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-14 23:10:19 -0300

    Update RandomNumberSupplier.java

--- src/main/java/com/myprojecticaro/poc_java_streams/supplier/RandomNumberSupplier.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/supplier/RandomNumberSupplier.java
@@ -1,1 +1,16 @@
+package com.example.streams.supplier;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class RandomNumberSupplier implements Supplier<Integer> {
+
+    private final Random random = new Random();
+
+    @Override
+    public Integer get() {
+        System.out.println("Gerando número...");
+        return random.nextInt(100);
+    }
+}
 
