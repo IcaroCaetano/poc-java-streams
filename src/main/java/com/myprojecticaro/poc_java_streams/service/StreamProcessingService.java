81b4723bdabd3085f831342741f1c17ba0deae76
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-14 23:15:10 -0300

    Update StreamProcessingService.java

--- src/main/java/com/myprojecticaro/poc_java_streams/service/StreamProcessingService.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/service/StreamProcessingService.java
@@ -1,1 +1,20 @@
 
+package com.example.streams.service;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamProcessingService {
+
+    public void process(
+            Supplier<Integer> supplier,
+            Consumer<Integer> consumer
+    ) {
+
+        Stream.generate(supplier) // uses Supplier
+              .filter(n -> n % 2 == 0) // only even numbers
+              .map(n -> n * 2)         // doubles the value
+              .limit(5)               // limits execution
+              .forEach(consumer);     // uses Consumer
+}
