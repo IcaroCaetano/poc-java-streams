29f5d2e0cff6443e6b92cd065be78998cfeac58e
IcaroCaetano <icaro.caetano@gmail.com>
2026-01-19 22:58:15 -0300

    feat: add runnable

--- src/main/java/com/myprojecticaro/poc_java_streams/runnable/HeavyTaskRunnable.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/runnable/HeavyTaskRunnable.java
@@ -1,4 +1,15 @@
 package com.myprojecticaro.poc_java_streams.runnable;
 
-public class HeavyTaskRunnable {
+public class HeavyTaskRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("⏳ Starting heavy task...");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("✅ Heavy task finalizing");
+    }
 }
