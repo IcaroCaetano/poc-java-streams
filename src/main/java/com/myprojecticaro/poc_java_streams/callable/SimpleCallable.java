24befce583789a6802a493ef16c24e46c77dad04
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-20 23:09:18 -0300

    Update SimpleCallable.java

--- src/main/java/com/myprojecticaro/poc_java_streams/callable/SimpleCallable.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/callable/SimpleCallable.java
@@ -1,1 +1,11 @@
 
+import java.util.concurrent.Callable;
+
+public class SimpleCallable implements Callable<String> {
+
+    @Override
+    public String call() {
+        System.out.println("🧠 Executando SimpleCallable...");
+        return "Resultado OK";
+    }
+}
