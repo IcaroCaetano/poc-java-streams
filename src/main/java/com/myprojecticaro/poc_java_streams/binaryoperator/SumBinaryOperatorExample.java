c08244cf1edb5f5f14dd2231df9e62adef0dc54c
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-04 23:16:33 -0300

    Update SumBinaryOperatorExample.java

--- src/main/java/com/myprojecticaro/poc_java_streams/binaryoperator/SumBinaryOperatorExample.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/binaryoperator/SumBinaryOperatorExample.java
@@ -6,6 +6,8 @@ import java.util.function.BinaryOperator;
 public class SumBinaryOperatorExample {
 
     public void execute() {
+       System.out.println("Inicio SumBinaryOperatorExample");
+        
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
         BinaryOperator<Integer> sumOperator = (a, b) -> a + b;
@@ -14,6 +16,8 @@ public class SumBinaryOperatorExample {
                 .reduce(0, sumOperator);
 
         System.out.println("Sum result: " + result);
+
+        System.out.println("Fim SumBinaryOperatorExample");
     }
 }
 
