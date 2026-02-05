4ffc0c3b9b24d96a1c6e5af5146e768f436b4fab
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-04 23:10:34 -0300

    Update StringConcatBinaryOperatorExample.java

--- src/main/java/com/myprojecticaro/poc_java_streams/binaryoperator/StringConcatBinaryOperatorExample.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/binaryoperator/StringConcatBinaryOperatorExample.java
@@ -1,1 +1,20 @@
+package com.myprojecticaro.poc_java_streams.binaryoperator;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class StringConcatBinaryOperatorExample {
+
+    public void execute() {
+        List<String> words = List.of("Java", "Streams", "BinaryOperator");
+
+        BinaryOperator<String> concatOperator = (a, b) -> a + " " + b;
+
+        String result = words.stream()
+                .reduce(concatOperator)
+                .orElse("");
+
+        System.out.println("Concatenation result: " + result);
+    }
+}
 
