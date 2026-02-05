86676f206367720359c1fd841404f33c5383cbe9
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-04 23:09:09 -0300

    Implement MaxBinaryOperatorExample to find max value

--- src/main/java/com/myprojecticaro/poc_java_streams/binaryoperator/MaxBinaryOperatorExample.java
+++ src/main/java/com/myprojecticaro/poc_java_streams/binaryoperator/MaxBinaryOperatorExample.java
@@ -1,1 +1,20 @@
+package com.myprojecticaro.poc_java_streams.binaryoperator;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class MaxBinaryOperatorExample {
+
+    public void execute() {
+        List<Integer> values = List.of(10, 40, 5, 30);
+
+        BinaryOperator<Integer> maxOperator = (a, b) -> a > b ? a : b;
+
+        int max = values.stream()
+                .reduce(maxOperator)
+                .orElseThrow();
+
+        System.out.println("Max value: " + max);
+    }
+}
 
