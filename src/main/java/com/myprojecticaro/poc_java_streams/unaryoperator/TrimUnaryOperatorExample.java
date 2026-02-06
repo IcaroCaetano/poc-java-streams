021ce10f5a0aff1d28611eca78b5db83cbb32192
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-06 09:06:47 -0300

    feat: add unioperator

---
+++ src/main/java/com/myprojecticaro/poc_java_streams/unaryoperator/TrimUnaryOperatorExample.java
@@ -0,0 +1,18 @@
+package com.myprojecticaro.poc_java_streams.unaryoperator;
+
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+public class TrimUnaryOperatorExample {
+
+    public void execute() {
+
+        List<String> inputs = List.of("  java  ", " streams ", " lambda ");
+
+        UnaryOperator<String> trim = String::trim;
+
+        inputs.stream()
+                .map(trim)
+                .forEach(System.out::println);
+    }
+}
