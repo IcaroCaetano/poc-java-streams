8fac16549d4fde84a8ad5f53c554e4b4b07d43d7
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-06 09:14:49 -0300

    feat: add unioperator

---
+++ src/main/java/com/myprojecticaro/poc_java_streams/unaryoperator/DomainObjectUnaryOperatorExample.java
@@ -0,0 +1,36 @@
+package com.myprojecticaro.poc_java_streams.unaryoperator;
+
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+public class DomainObjectUnaryOperatorExample {
+
+    public void execute() {
+
+        List<User> users = List.of(
+                new User("icaro", false),
+                new User("admin", false)
+        );
+
+        UnaryOperator<User> activateUser = user -> new User(user.name, true);
+
+        users.stream()
+                .map(activateUser)
+                .forEach(System.out::println);
+    }
+
+    static class User {
+        String name;
+        boolean active;
+
+        User(String name, boolean active) {
+            this.name = name;
+            this.active = active;
+        }
+
+        @Override
+        public String toString() {
+            return name + " | active=" + active;
+        }
+    }
+}
